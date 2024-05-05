f356911c2c6aed093ff72de005b1c5b3c1a11b8e
Floogen <31755155+Floogen@users.noreply.github.com>
2024-05-04 19:23:19 -0500

    Fixed eyes.png being required despite BodyContentPack.HideEyes being true

--- FashionSense/FashionSense.cs
+++ FashionSense/FashionSense.cs
@@ -1425,7 +1425,7 @@ namespace FashionSense
                     appearanceModel.Texture = contentPack.ModContent.Load<Texture2D>(contentPack.ModContent.GetInternalAssetName(Path.Combine(parentFolderName, textureFolder.Name, "body.png")).Name);
 
                     // Verify we are given the eyes texture and if so, track it
-                    if (!File.Exists(Path.Combine(textureFolder.FullName, "eyes.png")))
+                    if (appearanceModel.HideEyes is false && !File.Exists(Path.Combine(textureFolder.FullName, "eyes.png")))
                     {
                         Monitor.Log($"Unable to add body for {appearanceModel.Name} from {contentPack.Manifest.Name}: No associated eyes.png given", LogLevel.Warn);
                         continue;
